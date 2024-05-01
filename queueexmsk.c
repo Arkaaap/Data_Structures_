7e642c4b5774fe759d686564dc49b9cdf26165e0
Arka@p <99315231+Arkaaap@users.noreply.github.com>
2024-05-01 11:25:02 +0530

    Queue sk date 5/1/2024

---
+++ queueexmsk.c
@@ -0,0 +1,56 @@
+//queue 
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int d;
+    struct node *next ;
+};
+
+struct node *top,*p;
+
+void push (int d)
+{
+    struct node *newnwode = (struct node *)malloc(sizeof(struct node*));
+    newnwode->d = d;
+    newnwode->next = top;
+    if (top == NULL)
+    {
+        top = newnwode;
+    }
+    else {
+        p =top;
+        while (p->next!=NULL)
+        {
+            p =p->next;
+        }
+        newnwode->next = p->next;
+        p->next = newnwode;
+    }
+}
+
+
+void pop ()
+{
+    top = top->next;
+}
+
+void display ()
+{
+    p =top;
+    while (p!=NULL)
+    {
+        printf ("%d\t",p->d);
+        p = p->next;
+    }
+}
+
+int main ()
+{
+    push(1);
+    push(90);
+    push(34);
+    pop();
+    pop();
+    display();
+}
