800476f95dc1d8194e9acffcfba66bec7e5db752
Arka@p <99315231+Arkaaap@users.noreply.github.com>
2025-04-22 21:16:53 +0530

    Update dijkstra.c

--- dijkstra.c
+++ dijkstra.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define inf 999999
+#define INF 9999
+
 
 void dijkstra(int n, int s, int cost[10][10], int dist[10]) {
-    int i, j, v, min, visited[10], count = 1;
+    int i, j, count = 0, visited[10] = {0}, v, min;
 
-    for (i = 1; i <= n; i++) {
+    for (i = 0; i < n; i++) {
         visited[i] = 0;
         dist[i] = cost[s][i];
     }
@@ -13,58 +14,60 @@ void dijkstra(int n, int s, int cost[10][10], int dist[10]) {
     visited[s] = 1;
     dist[s] = 0;
 
-    while (count < n) {
-        min = inf;
-        for (i = 1; i <= n; i++) {
-            if (!visited[i] && dist[i] < min) {
+    while (count < n - 1) {
+        min = INF;
+        for (i = 0; i < n; i++) {
+            if (visited[i] == 0 && dist[i] < min) {
                 min = dist[i];
                 v = i;
             }
         }
-
         visited[v] = 1;
         count++;
 
-        for (i = 1; i <= n; i++) {
-            if (!visited[i] && dist[i] > dist[v] + cost[v][i]) {
+        for (i = 0; i < n; i++) {
+            if (visited[i] == 0 && dist[v] + cost[v][i] < dist[i]) {
                 dist[i] = dist[v] + cost[v][i];
             }
         }
     }
 }
 
-int main(void) {
-    int n, s, cost[10][10], i, j, dist[10];
-
-    printf("Enter the number of nodes in your graph: ");
-    scanf("%d", &n);
-
-    printf("\n-- Enter the adjacency matrix --\n");
-    printf("(Enter 999999 for no direct connection)\n");
-
-    for (i = 1; i <= n; i++) {
-        for (j = 1; j <= n; j++) {
-            printf("Enter cost from node %d to node %d: ", i, j);
-            scanf("%d", &cost[i][j]);
+int main (void)
+{
+    int i,j,n,s,cost[10][10],dist[10];
+    
+    printf ("Enter The no of nodes :");
+    scanf ("%d",&n);
+    
+    printf ("\n--Reading the cost adajceny matrix--\n");
+    
+    for (i=1;i<=n;i++)
+    {
+        for (j=1;j<=n;j++)
+        {
+            printf ("Enter The %d-> %d ",i,j);
+            scanf ("%d",&cost[i][j]);
+            if (cost[i][j] == 0)
+                cost[i][j] = INF;
         }
     }
-
-    printf("Enter the source node (1 to %d): ", n);
-    scanf("%d", &s);
-
-    if (s < 1 || s > n) {
-        printf("Invalid source node. It must be between 1 and %d.\n", n);
-        return -1;
+    
+    printf ("Enter The source node %d-->%d",(0),(n));
+    scanf ("%d",&s);
+    
+    dijkstra (n,s,cost,dist);
+    
+    printf ("\nPrinting The distance ...\n");
+    
+    for (i=1;i<=n;i++)
+    {
+        printf ("%d-->%d = %d\n",s,i,dist[i]);
     }
-
-    dijkstra(n, s, cost, dist);
-
-    printf("\n--- Shortest paths from node %d ---\n", s);
-    for (i = 1; i <= n; i++) {
-        if (i != s) {
-            printf("%d --> %d = %d\n", s, i, dist[i]);
-        }
-    }
-
-    return 0;
+    
+    return (0);
 }
+
+
+
+
