6c35abb3b9e11877e20d98575df7691d42d13430
Arka@p <99315231+Arkaaap@users.noreply.github.com>
2025-06-19 15:12:16 +0530

    Update dp[3]subsetsum.c

--- dp[3]subsetsum.c
+++ dp[3]subsetsum.c
@@ -1,3 +1,26 @@
+/* *Author* Arkaaap *Date* 19/06/2003 *problem description* Array is given and a sum is given let's say 11 we have to sum the subsets of the array such that subsets's sum should be equal to the sum 
+The problem is very much lookalike 0/1 knapsack where we are given two array's val , weight and we have to put the element in abag or knapsack such that the 
+    the m is not execceded of the bag and the profit will be maximized */
+
+
+/*Identification of 0/1 knapsack is similar to this subset problem .
+    1.choices are given either we pick an element or do'nt pick an elemnt 
+    2.sum and m is given 
+    3.optimized answer is required 
+    4. we have choices either we take an element or do not take an element in this case we either take a number or do not take a number 
+    5. there were two arrays value , weight array but the size is n similarly we have one array which is array itself which contains element of numbers 
+same shit n size even though array was taken two in knapsack we always take an n size array similarly in this case it is happening 
+    6. these were some of points indentifying problems how it's similar to knappsack 
+    
+    */
+
+
+
+
+
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
